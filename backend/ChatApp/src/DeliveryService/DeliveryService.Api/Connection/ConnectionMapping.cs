8a17069dd5231a2755bb4f5f56f63da870584209
swcuongtran <cuongtran@swiftlink.tech>
2025-11-11 15:07:00 +0700

    update

--- backend/ChatApp/src/DeliveryService/DeliveryService.Api/Connection/ConnectionMapping.cs
+++ backend/ChatApp/src/DeliveryService/DeliveryService.Api/Connection/ConnectionMapping.cs
@@ -1,6 +1,49 @@
-﻿namespace DeliveryService.Api.Connection
+﻿
+using System.Collections.Concurrent;
+
+namespace DeliveryService.Api.Connection
 {
-    public class ConnectionMapping
+    public class ConnectionMapping : IConnectionMapping
     {
+        private readonly ConcurrentDictionary<string, HashSet<string>> _connections =
+            new ConcurrentDictionary<string, HashSet<string>>();
+        public void Add(string UserId, string ConnectionId)
+        {
+            _connections.AddOrUpdate(UserId,
+                _ => new HashSet<string> { ConnectionId },
+                (_, existingConnections) =>
+                {
+                    lock (existingConnections)
+                    {
+                        existingConnections.Add(ConnectionId);
+                    }
+                    return existingConnections;
+                });
+        }
+
+        public IEnumerable<string> GetConnections(string UserId)
+        {
+            if (_connections.TryGetValue(UserId, out var connections))
+            {
+                
+                return connections.ToList();
+            }
+            return Enumerable.Empty<string>();
+        }
+
+        public void Remove(string UserId, string ConnectionId)
+        {
+            if (_connections.TryGetValue(UserId, out var connections))
+            {
+                lock (connections)
+                {
+                    connections.Remove(ConnectionId);
+                    if (connections.Count == 0)
+                    {
+                        _connections.TryRemove(UserId, out _);
+                    }
+                }
+            }
+        }
     }
 }
--- backend/ChatApp/src/DeliveryService/DeliveryService.Api/KafkaOptions.cs
+++ backend/ChatApp/src/DeliveryService/DeliveryService.Api/KafkaOptions.cs
@@ -2,5 +2,8 @@
 {
     public class KafkaOptions
     {
+        public const string Kafka = "Kafka";
+        public string BootstrapServers { get; set; } = string.Empty;
+        public string GroupId { get; set; } = "delivery-service-group";
     }
 }
