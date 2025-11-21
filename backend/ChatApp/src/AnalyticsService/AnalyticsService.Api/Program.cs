093a17a06d13a1a79b541370e95f46a6d832d638
swcuongtran <cuongtran@swiftlink.tech>
2025-11-21 16:58:32 +0700

    update

---
+++ backend/ChatApp/src/AnalyticsService/AnalyticsService.Api/Program.cs
@@ -0,0 +1,58 @@
+using AnalyticsService.Api.Workers;
+using AnalyticsService.Application.Queries;
+using AnalyticsService.Infrastructure.MongoDb;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Net.WebSockets;
+
+var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddSingleton<IMongoDbContext, MongoDbContext>();
+builder.Services.AddHostedService<StatsConsumer>();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(GetDailyStatsQuery).Assembly));
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = config["JWT_AUTHORITY"];
+        options.Audience = config["JWT_AUDIENCE"];
+        options.RequireHttpsMetadata = false;
+
+        options.BackchannelHttpHandler = new HttpClientHandler
+        {
+            ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+        };
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuers = new[]
+            {
+                options.Authority,
+                "http://localhost:8082/realms/chatapp"
+            },
+            ValidateAudience = true,
+            ValidAudiences = new[] { "account", "chatapp-api" },
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true
+        };
+    });
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ backend/ChatApp/src/AnalyticsService/AnalyticsService.Infrastructure/MongoDb/MongoDbContext.cs
@@ -0,0 +1,26 @@
+﻿using AnalyticsService.Infrastructure.MongoDb.Documents;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace AnalyticsService.Infrastructure.MongoDb
+{
+    public interface IMongoDbContext
+    {
+        IMongoCollection<DailyStatDocument> DailyStats { get; }
+    }
+    public class MongoDbContext : DbContext, IMongoDbContext
+    {
+        private readonly IMongoDatabase _database;
+        public MongoDbContext(IConfiguration configuration)
+        {
+            var connectionString = configuration["MONGO_CONNECTION_STRING"]
+                ?? throw new InvalidOperationException("MONGO_CONNECTION_STRING is missing.");
+            var databaseName = configuration["MONGO_DB_NAME"] ?? "chatapp_analytics";
+
+            var client = new MongoClient(connectionString);
+            _database = client.GetDatabase(databaseName);
+        }
+        public IMongoCollection<DailyStatDocument> DailyStats => _database.GetCollection<DailyStatDocument>("DailySystemStat");
+    }
+}
